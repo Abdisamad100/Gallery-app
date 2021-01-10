48245b9ac3ab24c54c6ec34b0638dee34ad8757b
abdisamad100 <abdisamadcade004@gmail.com>
2021-01-10 16:14:09 +0300

    added class location to models

--- gallery/app/models.py
+++ gallery/app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
+class Location(models.Model):
+    location_name = models.CharField(max_length=60)
+
+    def __str__(self):
+        '''
+        A string representation
+        '''
+        return self.location_name
+
+    def save_location(self):
+        '''
+        A method that saves the location name
+        '''
+        return self.save()
+
+    @classmethod
+    def delete_location(cls, id):
+        return cls.objects.filter(id = id).delete()
+
