86696bfbf8df7874c00709a0df694864e07c28fc
abdisamad100 <abdisamadcade004@gmail.com>
2021-01-10 10:30:59 +0300

    updated the views

--- gallery/app/views.py
+++ gallery/app/views.py
@@ -5,3 +5,7 @@ from django.http  import HttpResponse
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
+
+def photo_of_day(request):
+    date = dt.date.today()
+    return render(request, 'all-photos/today-photos.html', {"date": date,})   
