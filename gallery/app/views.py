411e863c969e427593e9581b73c0543ecf002a80
abdisamad100 <abdisamadcade004@gmail.com>
2021-01-10 10:38:48 +0300

    updated the views

--- gallery/app/views.py
+++ gallery/app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 
 # Create your views here.
@@ -6,6 +6,24 @@ from django.http  import HttpResponse
 def welcome(request):
     return render(request, 'welcome.html')
 
-def photo_of_day(request):
+def photos_of_day(request):
     date = dt.date.today()
     return render(request, 'all-photos/today-photos.html', {"date": date,})   
+
+
+
+def past_days_photos(request, past_date):
+
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(photos_of_day)
+
+    return render(request, 'all-photos/past-photos.html', {"date": date})
