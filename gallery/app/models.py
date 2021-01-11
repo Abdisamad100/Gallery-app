d8261e852d119b0d4a7b178990a40b8f1f32fe97
abdisamad100 <abdisamadcade004@gmail.com>
2021-01-11 11:48:14 +0300

    added search form and modified it

--- gallery/app/models.py
+++ gallery/app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import datetime as dt
 
 class Location(models.Model):
-    name = models.CharField(max_length=50,blank=True, null=True)
+    name = models.CharField(max_length=50)
 
     def save_location(self):
         self.save()
@@ -21,7 +21,7 @@ class Location(models.Model):
         return self.name
 
 class Category(models.Model):
-    name= models.CharField(max_length=50,blank=True, null=True)
+    name= models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
@@ -34,7 +34,7 @@ class Category(models.Model):
     
 
 class Image(models.Model):
-    name = models.CharField(max_length = 60,blank=True, null=True)
+    name = models.CharField(max_length = 60)
     date = models.DateTimeField(auto_now_add=True)
     description = models.TextField()
     image = models.ImageField(upload_to='images/',default='DEFAULT VALUE', blank=True, null=True)
